881dc4fc59b0900526bd41bf6acce60b6d3abd6d
Omit Kumar <omitkumar86@gmail.com>
2023-02-21 23:46:11 +0600

    Initial commit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -97,7 +97,7 @@ class _HomePageState extends State<HomePage> {
           Container(
             child:Column(
               children: [
-                Container(
+                androidVersions==input1?Container(
                   padding: EdgeInsets.all(15),
                   decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),
                   border: Border.all(color: Colors.black)),
@@ -125,51 +125,50 @@ class _HomePageState extends State<HomePage> {
                         ],
                       ),
                     ],
-                  ),),
-                // Container(
-                //   padding: EdgeInsets.all(15),
-                //   decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),
-                //       border: Border.all(color: Colors.black)),
-                //   child: Column(
-                //     children: [
-                //       Text("Output - 02", style: TextStyle(fontSize: 16,fontWeight: FontWeight.w600),),
-                //       SizedBox(height: 20),
-                //       Row(
-                //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                //         children: [
-                //           Text("${androidVersions[0].title}", style: TextStyle(fontSize: 14,fontWeight: FontWeight.w600),),
-                //           Text("${androidVersions[1].title}", style: TextStyle(fontSize: 14,fontWeight: FontWeight.w600),),
-                //           Text("                     "),
-                //           Text("${androidVersions[2].title}", style: TextStyle(fontSize: 14,fontWeight: FontWeight.w600),),
-                //         ],
-                //       ),
-                //       SizedBox(height: 15),
-                //       Row(
-                //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                //         children: [
-                //           Text("${androidVersions[3].title}", style: TextStyle(fontSize: 14,fontWeight: FontWeight.w600),),
-                //           Text("          "),
-                //           Text("${androidVersions[4].title}", style: TextStyle(fontSize: 14,fontWeight: FontWeight.w600),),
-                //           Text("${androidVersions[5].title}", style: TextStyle(fontSize: 14,fontWeight: FontWeight.w600),),
-                //         ],
-                //       ),
-                //       SizedBox(height: 15),
-                //       Row(
-                //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                //         children: [
-                //           Text("${androidVersions[6].title}", style: TextStyle(fontSize: 14,fontWeight: FontWeight.w600),),
-                //           Text("${androidVersions[7].title}", style: TextStyle(fontSize: 14,fontWeight: FontWeight.w600),),
-                //           Text("${androidVersions[8].title}", style: TextStyle(fontSize: 14,fontWeight: FontWeight.w600),),
-                //           Text("${androidVersions[9].title}", style: TextStyle(fontSize: 14,fontWeight: FontWeight.w600),),
-                //         ],
-                //       ),
-                //     ],
-                //   ),
-                // ),
-              ],
+                  ),):
+                  Container(
+                    padding: EdgeInsets.all(15),
+                    decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),
+                        border: Border.all(color: Colors.black)),
+                    child: Column(
+                      children: [
+                        Text("Output - 02", style: TextStyle(fontSize: 16,fontWeight: FontWeight.w600),),
+                        SizedBox(height: 20),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text("${androidVersions[0].title}", style: TextStyle(fontSize: 14,fontWeight: FontWeight.w600),),
+                            Text("${androidVersions[1].title}", style: TextStyle(fontSize: 14,fontWeight: FontWeight.w600),),
+                            Text("                     "),
+                            Text("${androidVersions[2].title}", style: TextStyle(fontSize: 14,fontWeight: FontWeight.w600),),
+                          ],
+                        ),
+                        SizedBox(height: 15),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text("${androidVersions[3].title}", style: TextStyle(fontSize: 14,fontWeight: FontWeight.w600),),
+                            Text("          "),
+                            Text("${androidVersions[4].title}", style: TextStyle(fontSize: 14,fontWeight: FontWeight.w600),),
+                            Text("${androidVersions[5].title}", style: TextStyle(fontSize: 14,fontWeight: FontWeight.w600),),
+                          ],
+                        ),
+                        SizedBox(height: 15),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text("${androidVersions[6].title}", style: TextStyle(fontSize: 14,fontWeight: FontWeight.w600),),
+                            Text("${androidVersions[7].title}", style: TextStyle(fontSize: 14,fontWeight: FontWeight.w600),),
+                            Text("${androidVersions[8].title}", style: TextStyle(fontSize: 14,fontWeight: FontWeight.w600),),
+                            Text("${androidVersions[9].title}", style: TextStyle(fontSize: 14,fontWeight: FontWeight.w600),),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
-
-          ),
         ]),
       ),
     );
